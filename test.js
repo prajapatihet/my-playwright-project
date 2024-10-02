badc9c20914c1b213727cf5b61c1a884cef8439a
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-10-02 14:38:23 +0530

    input field handles

--- test.js
+++ test.js
@@ -10,6 +10,12 @@ const { chromium } = require('playwright');
 
     // const button = await page.$x('//button[contains(@class, "submit-btn")]') -- used to select button which not easily   select using simple selector 
 
+    // await page.click('#submit-button') -- used to click on button having id submit-button
+
+    // await page.type('input[name="username"]', 'myusername'); --- type 'myusername' into the input field having name username
+
+    // await page.selectOption('#country-select', 'US') --selects the option with the value US
+
     await browser.close();
 })();
 
