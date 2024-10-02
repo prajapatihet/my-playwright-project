03a59578f2629f21ebe6cd8cdbb0edf5ff63baf3
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-10-02 14:31:36 +0530

    selectors

--- test.js
+++ test.js
@@ -5,5 +5,11 @@ const { chromium } = require('playwright');
     const page = await browser.newPage();
     await page.goto('https://prajapatihet.github.io');
     await page.screenshot({ path: 'screenshot.png' });
+
+    // const button = await page.$('.submit-btn') -- used to select first btn with class submit-btn
+
+    // const button = await page.$x('//button[contains(@class, "submit-btn")]') -- used to select button which not easily   select using simple selector 
+
     await browser.close();
 })();
+
